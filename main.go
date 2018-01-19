9ea29c47ad190bde98b26f7adf4315b7b48668c7
tp6gl4cj86 <tp6gl4cj86@gmail.com>
2018-01-19 18:33:30 +0800

    fix

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if string.Contains(message.Text,"test") {
+				if Contains(message.Text, "test", true) {
 					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()
 					if err != nil {
 						log.Print(err)
